a071a9bbfd9f5dd33d920be499a76581ee47d6b2
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-19 08:00:15 +0000

    [Fix]after_signup

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,11 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+  end
+
+  def new
+    @book = Book.new
   end
 
   def create
@@ -40,6 +45,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
