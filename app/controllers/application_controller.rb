de653fe1c53503ac7a662a7f3193800db29985e8
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-19 06:56:17 +0000

    [Start]5-2

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!, expect: [:top,:about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def after_sign_in_path_for(resource)
+    if current_user
+        flash[:notice] = "Signed in successfully."
+        user_path(current_user.id)
+     else
+        flash[:notice] = "Welcome! You have signed up successfully."
+        user_path(current_user.id)
+     end
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+end
