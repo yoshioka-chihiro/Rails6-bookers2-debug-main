c17cafc62fa1e17f8bbbf09fdd1bb1477be283aa
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-20 07:09:33 +0000

    [Add]comment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @newbook = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
